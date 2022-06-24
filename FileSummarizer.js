e594d0b4a0037994b0df13c45af3fce4bfa9cc34
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-06-23 21:11:29 -0500

    12. feat: Mock example #2: Getting summary files

--- FileSummarizer.js
+++ FileSummarizer.js
@@ -2,4 +2,12 @@ const fs = require('fs');
 
 const getFilesInDirectorySync = url => fs.readdirSync(url);
 
+const summarizeFilesInDirectorySync = directory => {
+    return getFilesInDirectorySync(directory).map(fileName => ({
+        directory,
+        fileName
+    }))
+}
+
 module.exports.getFilesInDirectorySync = getFilesInDirectorySync;
+module.exports.summarizeFilesInDirectorySync = summarizeFilesInDirectorySync;
--- test/FileSummarizer.test.js
+++ test/FileSummarizer.test.js
@@ -1,11 +1,24 @@
-const { getFilesInDirectorySync } = require('../FileSummarizer');
+const { getFilesInDirectorySync, summarizeFilesInDirectorySync } = require('../FileSummarizer');
 
-it("1. Getting a summary files in a specific directory", () => {
-    const summarizzingFiles = getFilesInDirectorySync('/home/miko/Descargas/Postman');
-    const recieved = [ 'Postman', 'app', 'log.txt' ];
-    
-    expect(summarizzingFiles).not.toHaveLength(0);
-    expect(summarizzingFiles).toHaveLength(3);
-    expect(summarizzingFiles.constructor).toBeTruthy();
-    expect(summarizzingFiles).toEqual(expect.arrayContaining(recieved));
+describe("Tests to manage files in a directory", () => {
+    it("1. Getting a summary files in a specific directory", () => {
+        const summarizzingFiles = getFilesInDirectorySync('/home/miko/Descargas/Postman');
+        const recieved = [ 'Postman', 'app', 'log.txt' ];
+        
+        expect(summarizzingFiles).not.toHaveLength(0);
+        expect(summarizzingFiles).toHaveLength(3);
+        expect(summarizzingFiles.constructor).toBeTruthy();
+        expect(summarizzingFiles).toEqual(expect.arrayContaining(recieved));
+    });
+
+    it("2. Summary files in a directory", () => {
+        const summary = summarizeFilesInDirectorySync('/home/miko/Descargas/Postman');
+        const recieved = [
+            { directory: '/home/miko/Descargas/Postman', fileName: 'Postman' },
+            { directory: '/home/miko/Descargas/Postman', fileName: 'app' },
+            { directory: '/home/miko/Descargas/Postman', fileName: 'log.txt' }
+        ]
+        expect(summary).toEqual(expect.arrayContaining(recieved));
+    })
+
 });
