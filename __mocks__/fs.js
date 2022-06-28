f69129bef4b6740fd75fd4d368763a686bcc1775
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-06-28 16:29:01 -0500

    12: feat: reading files of mock module

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -5,16 +5,19 @@ let mockFiles = Object.create({});
 
 const __setMockFiles = (newMockFiles) => {
     for(const file in newMockFiles){
-        const dir = path.dirname(file);
+        const dir = path.dirname(file); //Returns the path
         if(!mockFiles[dir]){
             mockFiles[dir] = [];
         }
-        mockFiles[dir].push(path.basename(file))
+        mockFiles[dir].push(path.basename(file)) //Returns the last part : filename
     }
 
     return mockFiles;
 }
 
+const readdirSync = path => mockFiles[path] || [];
+
 fs.__setMockFiles = __setMockFiles;
+fs.readdirSync = readdirSync;
 
 module.exports = fs;
--- test/example_2/__mocks__/fs.test.js
+++ test/example_2/__mocks__/fs.test.js
@@ -1,5 +1,7 @@
 jest.mock('fs');
 
+const fs = require('fs');
+
 describe("Tests for mocking module fs", () => {
     
     const MOCKED_FILE_INFO = {
@@ -8,9 +10,20 @@ describe("Tests for mocking module fs", () => {
     }
 
     it("1. Mocking files", () => {
-        const mockedFiles = require('fs').__setMockFiles(MOCKED_FILE_INFO);
+        const mockedFiles = fs.__setMockFiles(MOCKED_FILE_INFO);
         expect(mockedFiles).toEqual({
             '/path/to' : ['file1.js', 'file2.txt']
         })
     })
+
+    it("2. Reading files", () => {
+        const route = {
+            '/home/miko/Descargas/Postman/Postman' : 'uwu',
+            '/home/miko/Descargas/Postman/app' : 'uwu',
+            '/home/miko/Descargas/Postman/log.txt' : 'uwu'
+        }
+        fs.__setMockFiles(route);
+        const getFiles = fs.readdirSync('/home/miko/Descargas/Postman'); 
+        expect(getFiles).toEqual(expect.arrayContaining(['Postman', 'app', 'log.txt']));
+    });
 });
